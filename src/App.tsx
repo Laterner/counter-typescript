f500564ab88a6d35836a07b89485661445c72bea
Laterner <ivantrofimov2001@gmail.com>
2022-12-17 20:42:27 +0300

    create counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="round no-select">
+          <h1>{count}</h1>
+        </div>
+        <div className="button-wrapper no-select">
+          <button className="btn1" onClick={() => setCount(count - 1)}>-</button>
+          <button className="btn2" onClick={() => setCount(count + 1)}>+</button>
+        </div>
+        
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Нажимайте минус или плюс, чтобы уменишить или увеличить счётчик соответственно.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
